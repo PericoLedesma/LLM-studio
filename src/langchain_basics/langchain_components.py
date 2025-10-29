83f562b0ec74e6d5001a381cf49576718068f51f
Perico Ledesma <rgzledesma@gmail.com>
2025-10-29 15:47:37 +0100

    Components lanchain

--- src/langchain_basics/langchain_components.py
+++ src/langchain_basics/langchain_components.py
@@ -1,5 +1,5 @@
 from langchain_openai import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate, ChatPromptTemplate
 from langchain.chains import LLMChain
 
 
@@ -7,14 +7,23 @@ def generate_pet_name(animal_type: str = "Dog") -> str:
     # Component 1 - LLM
     llm = OpenAI(temperature=0.7)
 
-    # Component 2 - Prompt Templat
+    # Component 2 - Prompt Template
     prompt_template_name = PromptTemplate(
         input_variables=['animal_type'],
         template="You are a {animal_type} name generator. You will be given a description of a pet and you will generate a list of 5 names for the pet."
     )
 
+    # or 
+
+    template = ChatPromptTemplate(
+        [
+            ("system", "You are a helpful AI bot. Your name is Carl."),
+            ("human", "{user_input}"),
+        ]
+    )
+
     # Component 3 - Chain
-    # name_chain = prompt_template_name | llm Future
+    # name_chain =   prompt | llm | parser # alternative
     name_chain = LLMChain(llm=llm,
                           prompt=prompt_template_name)
 
